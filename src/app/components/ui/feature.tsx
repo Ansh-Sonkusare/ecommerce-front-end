69e1e8352c0e0982e86047e019ce032df7888a0e
zhi_jun <liangzhijun1216@gmail.com>
2024-02-06 21:04:30 -0500

    feat: customer support stuff

--- src/app/components/ui/feature.tsx
+++ src/app/components/ui/feature.tsx
@@ -1,33 +1,34 @@
 function Feature() {
-  return (
-    <div className="flex flex-col items-center">
-      <h2 className="font-inter mb-4 text-center text-3xl font-bold">
-        Feature Products
-      </h2>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="w-148 h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
-        <div className="w-148  h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
-        <div className="text-center">
-          <p className="mt-2">PixelPerfect Pro-35 DSLR Camera</p>
-          <p className="mt-2 text-gray-600">$10 USD</p>
-        </div>
-        <div className="text-center">
-          <p className="mt-2">PixelPerfect Pro-35 DSLR Camera</p>
-          <p className="mt-2  text-gray-600">$10 USD</p>
-        </div>
-        <div className="w-148  h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
-        <div className="w-148  h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
-        <div className="text-center">
-          <p className="mt-2">PixelPerfect Pro-35 DSLR Camera</p>
-          <p className="mt-2  text-gray-600">$10 USD</p>
-        </div>
-        <div className="text-center">
-          <p className="mt-2">PixelPerfect Pro-35 DSLR Camera</p>
-          <p className="mt-2  text-gray-600">$10 USD</p>
+    return (
+      <div className="flex flex-col items-center">
+        <h2 className="font-inter mb-4 text-center text-3xl font-bold">
+          Feature Products
+        </h2>
+        <div className="grid grid-cols-2 gap-10 p-14">
+          <div className="w-148 h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
+          <div className="w-148  h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
+          <div className="mb-4 text-center">
+            <p className="">PixelPerfect Pro-35 DSLR Camera</p>
+            <p className=" text-gray-600">$10 USD</p>
+          </div>
+          <div className="mb-4 text-center">
+            <p className="">PixelPerfect Pro-35 DSLR Camera</p>
+            <p className="  text-gray-600">$10 USD</p>
+          </div>
+          <div className="w-148  h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
+          <div className="w-148  h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
+          <div className=" mb-4 text-center">
+            <p className="">PixelPerfect Pro-35 DSLR Camera</p>
+            <p className="  text-gray-600">$10 USD</p>
+          </div>
+          <div className="mb-4 text-center">
+            <p className="">PixelPerfect Pro-35 DSLR Camera</p>
+            <p className=" text-gray-600">$10 USD</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-export default Feature;
+    );
+  }
+  
+  export default Feature;
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Navbar from "./components/ui/navbar";
 import Feature from "./components/ui/feature";
+import Deliver from "./components/ui/support";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,8 @@ export default function HomePage() {
       <Navbar />
 
       <Feature />
+
+      <Deliver />
     </>
   );
 }
