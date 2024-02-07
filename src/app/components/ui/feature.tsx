bcfa7fafe35f67f1752b7f473bc75b8acde1cffb
zhi_jun <liangzhijun1216@gmail.com>
2024-02-06 19:40:40 -0500

    feat: feature product

---
+++ src/app/components/ui/feature.tsx
@@ -0,0 +1,33 @@
+function Feature() {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="font-inter mb-4 text-center text-3xl font-bold">
+        Feature Products
+      </h2>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="w-148 h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
+        <div className="w-148  h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
+        <div className="text-center">
+          <p className="mt-2">PixelPerfect Pro-35 DSLR Camera</p>
+          <p className="mt-2 text-gray-600">$10 USD</p>
+        </div>
+        <div className="text-center">
+          <p className="mt-2">PixelPerfect Pro-35 DSLR Camera</p>
+          <p className="mt-2  text-gray-600">$10 USD</p>
+        </div>
+        <div className="w-148  h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
+        <div className="w-148  h-48  rounded-lg border border-gray-300 bg-white p-6 shadow-md hover:scale-105 hover:shadow-lg"></div>
+        <div className="text-center">
+          <p className="mt-2">PixelPerfect Pro-35 DSLR Camera</p>
+          <p className="mt-2  text-gray-600">$10 USD</p>
+        </div>
+        <div className="text-center">
+          <p className="mt-2">PixelPerfect Pro-35 DSLR Camera</p>
+          <p className="mt-2  text-gray-600">$10 USD</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feature;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import Navbar from "./components/ui/navbar";
+import Feature from "./components/ui/feature";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
+
+      <Feature />
     </>
   );
 }
