07d15731928ba59ca6ffc5b36cd096cade064c88
Ansh-Sonkusare <sonkusare.satish12@gmail.com>
2024-02-14 01:20:38 +0530

    feat: Footer

--- src/app/components/ui/footer.tsx
+++ src/app/components/ui/footer.tsx
@@ -2,37 +2,45 @@ import Image from "next/image";
 
 function Footer() {
   return (
-    <div className="flex flex-row gap-10 p-14">
-      <p>Terms of Services </p>
-      <p>Privacy Policy</p>
-      <input
-        className=" type-'email id-'input-9' class-'w-full' h-10 rounded rounded-r-none border border-r-0 px-3 text-sm  text-gray-700 focus:outline-none "
-        placeholder="user@mail.com"
-      />
-      <button className="rounded-lg bg-[#2F2E2E] px-4 py-2 text-white h-10">
-        Subscribe
-      </button>
-      <Image
-        src={"/facebook-logo-498.jpg"}
-        alt={"facebook"}
-        height={40}
-        width={40}
-        className="h-10"
-      ></Image>
-      <Image
-        src={"/logo-ig-png-32464.png"}
-        alt={"instagram"}
-        height={40}
-        width={40}
-        className="h-10"
-      ></Image>
-      <Image
-        src={"/logo-twitter-png-5860.png"}
-        alt={"twitter"}
-        height={40}
-        width={40}
-        className="h-10"
-      ></Image>
+    <div className="justify-centre px-30 mb-2 mt-6 flex border-t-2 pt-4">
+      <div className="flex w-full items-center justify-between px-10">
+        <div className="flex gap-6">
+          <p>Terms of Services</p>
+          <p>Privacy Policy</p>
+        </div>
+
+        <div className="flex gap-6">
+          <input
+            type="email"
+            className=" h-10 rounded  border-2  border-gray-300 px-3 text-sm  text-gray-700 focus:outline-none "
+            placeholder="user@mail.com"
+          />
+          <button className="h-10 rounded-lg bg-[#2F2E2E] px-4 py-2 text-white">
+            Subscribe
+          </button>
+          <Image
+            src={"/facebook-logo-498.jpg"}
+            alt={"facebook"}
+            height={40}
+            width={40}
+            className="h-10"
+          ></Image>
+          <Image
+            src={"/logo-ig-png-32464.png"}
+            alt={"instagram"}
+            height={40}
+            width={40}
+            className="h-10"
+          ></Image>
+          <Image
+            src={"/logo-twitter-png-5860.png"}
+            alt={"twitter"}
+            height={40}
+            width={40}
+            className="h-10"
+          ></Image>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Feature from "./components/ui/feature";
 import Support from "./components/ui/supportPage";
 import Title from "./components/ui/title";
 import Promo from "./components/ui/promo";
+import Footer from "./components/ui/footer";
 
 export default function HomePage() {
   return (
@@ -17,6 +18,8 @@ export default function HomePage() {
       <Promo />
 
       <Support />
+
+      <Footer />
     </>
   );
 }
